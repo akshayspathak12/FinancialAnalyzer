27d0e60c85c844380bc2d49e6cead170fd7b384d
akshayspathak12 <84868918+akshayspathak12@users.noreply.github.com>
2021-08-03 09:57:13 +0530

    Add files via upload

--- financial_Analyzer/src/main/java/com/example/financial_Analyzer/Service/UserServiceImpl.java
+++ financial_Analyzer/src/main/java/com/example/financial_Analyzer/Service/UserServiceImpl.java
@@ -6,6 +6,7 @@ import java.util.List;
 import javax.transaction.TransactionalException;
 
 import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.interceptor.AbstractFallbackTransactionAttributeSource;
@@ -26,98 +27,118 @@ public class UserServiceImpl implements UserService {
 
 	@Autowired
 	private TransactionDetailRepo transactionRepo;
-	
+
 	@Autowired
 	private MonthlySpendRepository monthlySpendRepo;
-	
-	
-	//adds the entries to the DB using ORM
+
+	private static final Logger LOGGER = LoggerFactory.getLogger(UserServiceImpl.class);
+
+	// adds the entries to the DB using ORM
 	@Override
 	public TransactionDetail saveData(TransactionDetailDto detail) throws Exception {
-		if(detail.getCustomer_Id()==0||detail.getTransaction_Description()==""||detail.getAmount()==0||detail.getPayment_Type()==""||detail.getCategory()=="")
+		if (detail.getCustomer_Id() == 0 || detail.getTransaction_Description() == "" || detail.getAmount() == 0
+				|| detail.getPayment_Type() == "" || detail.getCategory() == "") {
+			LOGGER.info("********** Somthing went wrong *************");
 			throw new Exception("enter valid details");
-		TransactionDetail save = transactionRepo.save(new TransactionDetail(detail.getCustomer_Id(),detail.getTransaction_Description() ,detail.getAmount() , detail.getPayment_Type(), detail.getCategory(), detail.getDate()));
+		}
+		TransactionDetail save = transactionRepo
+				.save(new TransactionDetail(detail.getCustomer_Id(), detail.getTransaction_Description(),
+						detail.getAmount(), detail.getPayment_Type(), detail.getCategory(), detail.getDate()));
 		return save;
 	}
 
-
-	//provides the list of transactions
+	// provides the list of transactions
 	@Override
-	public List<TransactionDetailDto> findAllData() {
+	public List<TransactionDetailDto> findAllData() throws TransactionsNotFoundException {
 		List<TransactionDetailDto> list = new ArrayList<>();
-		
+
 		List<TransactionDetail> findAll = transactionRepo.findAll();
 		for (TransactionDetail transactionDetail : findAll) {
-			list.add(new TransactionDetailDto(transactionDetail.getCustomer_Id(), transactionDetail.getTransaction_Description(), transactionDetail.getAmount(), transactionDetail.getPayment_Type(), transactionDetail.getCategory(),transactionDetail.getDate()));
-			
+			list.add(new TransactionDetailDto(transactionDetail.getCustomer_Id(),
+					transactionDetail.getTransaction_Description(), transactionDetail.getAmount(),
+					transactionDetail.getPayment_Type(), transactionDetail.getCategory(), transactionDetail.getDate()));
+
 		}
 		if (!list.isEmpty()) {
 			return list;
-		}
-		else {
+		} else {
+			LOGGER.info("******** No Transaction Available *********");
 			throw new TransactionsNotFoundException("No Transaction Available");
-			
+
 		}
-		
+
 	}
 
-	//add monthly data
+	// add monthly data
 	@Override
 	public MonthlySpend saveMonthlyData(MonthlySpendDto detail) throws Exception {
-		if(detail.getTotal_Incoming()==0.0||detail.getCustomer_Number()==0.0||detail.getTotal_Outgoing()==0.0||detail.getClosing_balance()==0.0||detail.getMonth()=="")
+		if (detail.getTotal_Incoming() == 0.0 || detail.getCustomer_Number() == 0.0 || detail.getTotal_Outgoing() == 0.0
+				|| detail.getClosing_balance() == 0.0 || detail.getMonth() == "") {
+			LOGGER.info("********** Somthing went wrong *************");
 			throw new Exception("enter valid details");
-		MonthlySpend save = monthlySpendRepo.save(new MonthlySpend(detail.getMonth(), detail.getCustomer_Number(), detail.getTotal_Incoming(), detail.getTotal_Outgoing(), detail.getClosing_balance()));
-		return save;
+		} else {
+			MonthlySpend save = monthlySpendRepo.save(new MonthlySpend(detail.getMonth(), detail.getCustomer_Number(),
+					detail.getTotal_Incoming(), detail.getTotal_Outgoing(), detail.getClosing_balance()));
+			return save;
+		}
 	}
 
-
-	//returns all transaction details for provided income source
+	// returns all transaction details for provided income source
 	@Override
 	public List<TransactionDetailDto> findIncomeData(String paymentCategory) {
 		List<TransactionDetailDto> list = new ArrayList<>();
 		List<TransactionDetail> findIncome = transactionRepo.findIncome(paymentCategory);
 		for (TransactionDetail transactionDetail : findIncome) {
 			if (transactionDetail.getPayment_Type().equals("credit")) {
-				list.add(new TransactionDetailDto(transactionDetail.getCustomer_Id(), transactionDetail.getTransaction_Description(), transactionDetail.getAmount(), transactionDetail.getPayment_Type(), transactionDetail.getCategory(),transactionDetail.getDate()));
+				list.add(new TransactionDetailDto(transactionDetail.getCustomer_Id(),
+						transactionDetail.getTransaction_Description(), transactionDetail.getAmount(),
+						transactionDetail.getPayment_Type(), transactionDetail.getCategory(),
+						transactionDetail.getDate()));
 			}
 		}
 		if (list.isEmpty()) {
+			LOGGER.info("******** No Transaction Available for " + paymentCategory + " **********");
 			throw new IncomeNotFoundException("No Income Transaction Available");
 		}
 		return list;
 	}
 
-
-	//returns all expenses as per provided expense type
+	// returns all expenses as per provided expense type
 	@Override
 	public List<TransactionDetailDto> findExpenseData(String paymentCategory) {
 		List<TransactionDetailDto> list = new ArrayList<>();
 		List<TransactionDetail> findIncome = transactionRepo.findIncome(paymentCategory);
 		for (TransactionDetail transactionDetail : findIncome) {
 			if (transactionDetail.getPayment_Type().equals("debit")) {
-				list.add(new TransactionDetailDto(transactionDetail.getCustomer_Id(), transactionDetail.getTransaction_Description(), transactionDetail.getAmount(), transactionDetail.getPayment_Type(), transactionDetail.getCategory(),transactionDetail.getDate()));
+				list.add(new TransactionDetailDto(transactionDetail.getCustomer_Id(),
+						transactionDetail.getTransaction_Description(), transactionDetail.getAmount(),
+						transactionDetail.getPayment_Type(), transactionDetail.getCategory(),
+						transactionDetail.getDate()));
 			}
 		}
 		if (list.isEmpty()) {
+			LOGGER.info("******** No Transaction Available for " + paymentCategory + " **********");
 			throw new ExpensesNotFoundException("No Expenses Transaction Available");
 		}
 		return list;
 	}
 
-
-	//returns monthly data 
+	// returns monthly data
 	@Override
 	public List<MonthlySpendDto> findAllMonthlyData() {
 		List<MonthlySpendDto> list = new ArrayList<>();
 		List<MonthlySpend> findAll = monthlySpendRepo.findAll();
 		for (MonthlySpend monthlyDetail : findAll) {
-			list.add(new MonthlySpendDto(monthlyDetail.getMonth(), monthlyDetail.getCustomer_Number(), monthlyDetail.getTotal_Incoming(), monthlyDetail.getTotal_Outgoing(), monthlyDetail.getClosing_balance()));
+			list.add(new MonthlySpendDto(monthlyDetail.getMonth(), monthlyDetail.getCustomer_Number(),
+					monthlyDetail.getTotal_Incoming(), monthlyDetail.getTotal_Outgoing(),
+					monthlyDetail.getClosing_balance()));
 		}
 		if (list.isEmpty()) {
+			LOGGER.info("******** No Monthly Transaction Available somthing went wrong  **********");
 			throw new MonthlyDataNotFoundException("No Monthly Transaction Available");
 		}
 		return list;
-		
+
 	}
 
 }
